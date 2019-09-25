d32f0e0b356d4174bc30d49d3193b166bf945bb1
gerardo jerez <gerardo.jerez@hotmail.com>
2019-09-25 15:50:28 -0700

    antes del cagadero

--- characters.js
+++ characters.js
@@ -9,15 +9,24 @@ let getCharacters = async ()=>{
     const response4 = await fetch(people3.info.next);
     const people4 = await response4.json();
     console.log(people);*/
-    
-    people.results.forEach(element =>{
+    let flag = 0;
+    people.results.some(element =>{
+        
         const imgChar = element.image;
-        const namechar = element.name;
-        const statuschar = element.status;
-        const specieschar = element.species;
-        const genderchar = element.gender;  
-        character.innerHTML += `<img src="${imgChar}" class="img-char"> `
-        character.innerHTML += `<p> Name: ${namechar} </p> <p> Status: ${statuschar}</p> <p> Specie: ${specieschar}</p> <p> Gender: ${genderchar}</p> </div>`
+        const nameChar = element.name;
+        const statusChar = element.status;
+        const speciesChar = element.species;
+        const genderChar = element.gender;
+        flag++;
+        character.innerHTML += `<a href="infoChar/char${flag}.html"><img src="${imgChar}" class="img-char"></a>`;
+        
+        /*if(flag === 10){
+            return element === element;
+        }*/
+        
+        //character.innerHTML += `<p> Name: ${nameChar} </p> <p> Status: ${statusChar}</p> <p> Specie: ${speciesChar}</p> <p> Gender: ${genderChar}</p> </div>`
+        
+        
     });
     /*people2.results.forEach(element =>{
         const imgChar = element.image;
