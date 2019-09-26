7d4cf2840ec467c2f4587d7758b3a5c307fbd4c2
Moises Alberto Rodriguez Aceves <mr@MacBook-Pro-de-Moises.local>
2019-09-25 21:55:43 -0700

    holi

--- infoChar/char.js
+++ infoChar/char.js
@@ -1,13 +1,13 @@
-let flag = ; 
-
+const char = document.getElementById("char"); 
 let getcharacters = async() =>{
+    //const identificacion = document.getElementById('1');
     const response = await fetch ('https://rickandmortyapi.com/api/character/');
     const personaje = await response.json();
     console.log(personaje.results[2]); 
 
     const {image , name , status , species , gender } = personaje.results[0];
-    console.log(name , status); 
-    character.innerHTML += '<img src="${imgChar}" class="img-char"></a>`;
+    console.log(name , status, species, gender, image); 
+    char.innerHTML += `<img src="${image}" class="image"></a>` ; 
 
 
 
