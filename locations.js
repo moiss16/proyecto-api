3268f570a82511813aafac3acd65480c410715e7
gerardo jerez <gerardo.jerez@hotmail.com>
2019-09-26 15:31:23 -0700

    cambios menores

--- locations.js
+++ locations.js
@@ -2,11 +2,22 @@ const locationInfo = document.getElementById('location');
 let getLocaUrl = async () => {
     const response = await fetch('https://rickandmortyapi.com/api/location');
     const place = await response.json();
+    let flag = 0;
     place.results.forEach(element =>{
-        
-        locationInfo.innerHTML += `<h1>${element.name}</h1>`;
+        locationInfo.innerHTML += `
+        <ul>
+            <h1>${element.name}</h1>
+            <li>Dimension: ${element.dimension}</li>
+            <li>Type: ${element.type}<br></li>
+        </ul>
+        <div class="img-conta">
+        <img src="imagenes/The_Smith_Residence.webp" class="img-loca">
+        </div>
+        `;
+
+
     })
-    console.log(place);
+    
     
 }
 getLocaUrl();
